c7bd2d691e32c64673ff06fb859bf1608422531d
Agent Dyke <benjaminzuze06@gmail.com>
2025-12-11 13:37:40 +0200

    Add product list with details in script.js

---
+++ script.js
@@ -0,0 +1,22 @@
+  const products = [
+    { name: 'iPhone X', price: '180', image: 'images/iphone-x.jpg' ,
+     name: 'iPhone XR', price: '210', image: 'images/iphone-xr.jpg' },
+    { name: 'iPhone 11 64GB', price: '240', image: 'images/iphone11-64.jpg' ,
+     name: 'iPhone 11 128GB', price: '260', image: 'images/iphone11-128.jpg' },
+    { name: 'iPhone 11 Pro Max', price: '330', image: 'images/iphone11promax.jpg' ,
+     name: 'iPhone 12', price: '280', image: 'images/iphone12.jpg' },
+    { name: 'iPhone 12 Pro', price: '380', image: 'images/iphone12pro.jpg' ,
+     name: 'iPhone 12 Pro Max 128GB', price: '390', image: 'images/iphone12promax-128.jpg' },
+    { name: 'iPhone 12 Pro Max 256GB', price: '430', image: 'images/iphone12promax-256.jpg' ,
+     name: 'iPhone 13 128GB', price: '370', image: 'images/iphone13-128.jpg' },
+    { name: 'iPhone 13 256GB', price: '400', image: 'images/iphone13-256.jpg' ,
+  [ name: 'iPhone 13 Pro', price: '460', image: 'images/iphone13pro.jpg' },
+    { name: 'iPhone 13 Pro Max 128GB', price: '490', image: 'images/iphone13promax-128.jpg' ,
+     name: 'iPhone 13 Pro Max 256GB', price: '520', image: 'images/iphone13promax-256.jpg' },
+    { name: 'iPhone 14', price: '460', image: 'images/iphone14.jpg' ,
+     name: 'iPhone 14 Plus', price: '470', image: 'images/iphone14plus.jpg' },
+    { name: 'iPhone 14 Pro', price: '550', image: 'images/iphone14pro.jpg' ,
+     name: 'iPhone 14 Pro Max 128GB', price: '580', image: 'images/iphone14promax-128.jpg' },
+    { name: 'iPhone 14 Pro Max 256GB', price: '600', image: 'images/iphone14promax-256.jpg' ,
+     name: 'iPhone 15 Pro Max', price: '850', image: 'images/iphone15promax.jpg' }
+  ];
